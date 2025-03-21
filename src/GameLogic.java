19fef254fe4d33a50e24a09e581f3d2693ab8200
BT_1N3_05 <bths@test.com>
2025-03-21 10:10:22 -0400

    Day 5: Current Progress

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+
 public class GameLogic {
     int[][] arr = new int[10][20];
-    Shape[][] shapeArr;
+    ArrayList<Coordinate> cords;
+    Shape[] shapeArr;
 
     public GameLogic() {
+        shapeArr = new Shape[]{new ITetromino(), new OTetromino(), new TTetromino(), new STetromino(), new ZTetromino(), new JTetromino(), new LTetromino()};
+        cords = new ArrayList<>();
+        generateGridBox();
     }
 
+    public void generateGridBox() {
+        for (int i = 0; i <= 270; i+=30) {
+            for (int j = 0; j <= 570; j+= 30) {
+                cords.add(new Coordinate(i, j, i+30, j+30));
+            }
+        }
+    }
 
+    public void printCords() {
+        for (Coordinate cord : cords) {
+            System.out.println("(" + cord.getX1() + ", " + cord.getY1() + "); " + "(" + cord.getX2() + ", " + cord.getY2() + ")");
+        }
+        System.out.println(cords.size());
+    }
 }
--- src/TetrisRunner.java
+++ src/TetrisRunner.java
@@ -1,5 +1,7 @@
 public class TetrisRunner {
     public static void main(String[] args) {
         TetrisFrame frame = new TetrisFrame();
+        GameLogic hi = new GameLogic();
+        hi.printCords();
     }
 }
