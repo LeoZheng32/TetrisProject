5f160bb893ae181fb0dcc64fd40f9e91d8e3c3f7
BT_2N2_05 <bths@test.com>
2025-03-27 16:33:33 -0400

    3/27/2025

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,12 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class GameLogic {
-    int[][] arr = new int[20][10];
+public class GameLogic implements ActionListener {
+    int[][] boardArr = new int[20][10];
     int indexOfStartingRow;
     int indexOfStartingCol;
     ArrayList<Coordinate> currentFallingBlock;
     ArrayList<Coordinate> cords;
     Shape[] shapeArr;
+    private Timer timer;
+    private boolean paused = false;
 
 
     public GameLogic() {
@@ -17,6 +22,8 @@ public class GameLogic {
         indexOfStartingCol = 3;
         generateGridBox();
         generateBlock();
+        timer = new Timer(1000, this);
+        timer.start();
     }
 
     public void generateBlock() {
@@ -24,8 +31,8 @@ public class GameLogic {
         int[][] shape = shapeArr[idx].getShape();
         for (int row = 0; row < shape.length; row++) {
             for (int col = 0; col < shape[0].length; col++) {
-                arr[indexOfStartingRow+row][indexOfStartingCol+col] = shape[row][col];
-                if (arr[indexOfStartingRow+row][indexOfStartingCol+col] != 0) {
+                boardArr[indexOfStartingRow+row][indexOfStartingCol+col] = shape[row][col];
+                if (boardArr[indexOfStartingRow+row][indexOfStartingCol+col] != 0) {
                     currentFallingBlock.add(new Coordinate(indexOfStartingRow+row, indexOfStartingCol+col));
                 }
             }
@@ -35,29 +42,47 @@ public class GameLogic {
 
 
     public void updateFallingBlock(String direction) {
-        int blockNum = arr[currentFallingBlock.getFirst().getRowIdx()][currentFallingBlock.getFirst().getColIdx()];
+        int blockNum = boardArr[currentFallingBlock.getFirst().getRowIdx()][currentFallingBlock.getFirst().getColIdx()];
         for (Coordinate cord : currentFallingBlock) {
-            arr[cord.getRowIdx()][cord.getColIdx()] = 0;
+            boardArr[cord.getRowIdx()][cord.getColIdx()] = 0;
         }
         for (Coordinate cord : currentFallingBlock) {
             int row = cord.getRowIdx();
             int col = cord.getColIdx();
             if (direction.equals("down")) {
-                arr[row + 1][col] = blockNum;
+                boardArr[row + 1][col] = blockNum;
                 cord.decrementRowIdx();
             } else if (direction.equals("left")) {
-                arr[row][col-1] = blockNum;
+                boardArr[row][col-1] = blockNum;
                 cord.setColIdx(-1);
             } else if (direction.equals("right")) {
-                arr[row][col+1] = blockNum;
+                boardArr[row][col+1] = blockNum;
                 cord.setColIdx(1);
             }
         }
     }
 
     public boolean canMoveLeft() {
-        for (Coordinate cord : currentFallingBlock) {
-            if (cord.getColIdx() == 0) {
+        ArrayList<Integer> uniqueRowIdx = new ArrayList<>();
+        ArrayList<Integer> lowestColIdxPerRow = new ArrayList<>();
+        for (int i = 0; i < currentFallingBlock.size(); i++) {
+            if (!uniqueRowIdx.contains(currentFallingBlock.get(i).getRowIdx())) {
+                uniqueRowIdx.add(currentFallingBlock.get(i).getRowIdx());
+            }
+        }
+        for (int rowIdx : uniqueRowIdx) {
+            int lowestColIdx = 20;
+            for (Coordinate cord : currentFallingBlock) {
+                if (cord.getColIdx() < lowestColIdx) {
+                    lowestColIdx = cord.getColIdx();
+                }
+            }
+            lowestColIdxPerRow.add(lowestColIdx);
+        }
+        for (int i = 0; i < lowestColIdxPerRow.size(); i++) {
+            if (lowestColIdxPerRow.get(i) == 0) {
+                return false;
+            } else if (boardArr[uniqueRowIdx.get(i)][lowestColIdxPerRow.get(i)-1] != 0) {
                 return false;
             }
         }
@@ -75,9 +100,9 @@ public class GameLogic {
 
 
     public void printArr() {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                System.out.print(arr[i][j]);
+        for (int i = 0; i < boardArr.length; i++) {
+            for (int j = 0; j < boardArr[0].length; j++) {
+                System.out.print(boardArr[i][j]);
             }
             System.out.println();
         }
@@ -96,5 +121,11 @@ public class GameLogic {
                 cords.add(new Coordinate(row, col, row+30, col +30));
             }
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        paused = !paused;
+        //System.out.println(time);
     }
 }
